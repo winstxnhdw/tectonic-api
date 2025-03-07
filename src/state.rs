82f76c38b9c996be2bf68951045d3ecaa7594c30
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-03-07 09:20:08 +0000

    chore: use hash builder directly

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,4 @@
-use gxhash::GxBuildHasher;
-
 #[derive(Clone)]
 pub struct AppState {
-    pub cache: moka::future::Cache<String, Vec<u8>, GxBuildHasher>,
+    pub cache: moka::future::Cache<String, Vec<u8>, gxhash::GxBuildHasher>,
 }
