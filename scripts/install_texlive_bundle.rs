43517d8dd7de6aa6a4c992b1cf8c6e3d35de8b1e
winstxnhdw <winstxnhdw@gmail.com>
2026-01-03 09:43:21 +0800

    build: use external harfbuzz

--- scripts/install_texlive_bundle.rs
+++ scripts/install_texlive_bundle.rs
@@ -5,20 +5,19 @@ name = "install-texlive-bundle"
 version = "0.1.0"
 edition = "2024"
 
-[dependencies]
-tectonic = { git = "https://github.com/tectonic-typesetting/tectonic.git", default-features = false, features = ["geturl-reqwest"] }
+[dependencies.tectonic]
+git = "https://github.com/tectonic-typesetting/tectonic.git"
+default-features = false
+features = ["geturl-reqwest", "external-harfbuzz"]
 ---
 
 fn main() -> tectonic::errors::Result<()> {
     let mut status = tectonic::status::NoopStatusBackend::default();
     let mut bundle = tectonic::config::PersistentConfig::open(false)?.default_bundle(false)?;
 
-    bundle.all_files().iter().for_each(|name| {
-        match bundle.input_open_name(name, &mut status).must_exist() {
-            Ok(_) => println!("Cached {}", name),
-            Err(e) => eprintln!("Failed {}: {}", name, e),
-        }
-    });
+    for name in bundle.all_files() {
+        bundle.input_open_name(&name, &mut status).must_exist()?;
+    }
 
     Ok(())
 }
