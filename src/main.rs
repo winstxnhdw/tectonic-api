216debac8bfe8ca11e5b307bdfedf0e25a776690
winstxnhdw <winstxnhdw@gmail.com>
2026-01-03 17:11:13 +0800

    refactor: remove last `unwrap`

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use std::{env::var, time::Duration};
+use std::env::var;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let max_cache_memory = var("MAX_CACHE_MEMORY")
-        .unwrap_or("12884901888".into())
-        .parse()
-        .unwrap();
+        .ok()
+        .and_then(|key| key.parse().ok())
+        .unwrap_or(12884901888);
     let cache_expiry = var("CACHE_EXPIRY")
-        .unwrap_or("3600".into())
-        .parse()
-        .unwrap();
+        .ok()
+        .and_then(|key| key.parse().ok())
+        .unwrap_or(3600);
 
+    let app = tectonic_api::app(max_cache_memory, std::time::Duration::from_secs(cache_expiry));
     let port = var("SERVER_PORT").unwrap_or("5555".into());
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;
-    let app = tectonic_api::app(max_cache_memory, Duration::from_secs(cache_expiry));
 
     axum::serve(listener, app.into_make_service()).await
 }
